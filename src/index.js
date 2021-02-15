d1180191bd9273d3af531b5c124aac53448e3e45
Artyom <43495856+Artyom10@users.noreply.github.com>
2021-02-15 18:58:34 +0300

    small changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as $ from 'jquery';
-import Post from './Post';
-//import './styles/styles.css';
-import './styles/yup.scss';
-import WebpackLogo from './assets/webpack-x.png';
+import './styles/style.scss';
+
+
 
