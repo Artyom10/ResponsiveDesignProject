a9e6b8de02cb7f52d7a181e1a1393cd8a73c733b
Artsiom Hryshyn <Artsiom.Hryshyn@itechart-group.com>
2021-02-18 17:21:11 +0300

    save commit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import './styles/mainStyles.scss';
 //import './styles/styles.scss';
 
 
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+   return console.log('menu');
+}
